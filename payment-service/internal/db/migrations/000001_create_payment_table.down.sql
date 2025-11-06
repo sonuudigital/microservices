d7291ec12a455de1fdcca0fb5ec6ff402caadd70
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-11-06 10:43:33 -0300

    feat(payment-service): add Dockerfile and docker-compose configuration for payment service, including database setup and health checks

--- payment-service/internal/db/migrations/000001_create_payment_table.down.sql
+++ payment-service/internal/db/migrations/000001_create_payment_table.down.sql
@@ -1,3 +1,5 @@
-DROP TABLE IF EXISTS payment_statuses;
+DROP TRIGGER IF EXISTS trigger_set_default_payment_status ON payments;
+DROP FUNCTION IF EXISTS set_default_payment_status();
 DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS payment_statuses;
 DROP EXTENSION IF EXISTS "uuid-ossp";
