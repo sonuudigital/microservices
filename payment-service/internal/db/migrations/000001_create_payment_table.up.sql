e094c11277215eb26c0113796866160e41a4a7bf
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-11-06 10:43:33 -0300

    feat(payment-service): add Dockerfile and docker-compose configuration for payment service, including database setup and health checks

--- payment-service/internal/db/migrations/000001_create_payment_table.up.sql
+++ payment-service/internal/db/migrations/000001_create_payment_table.up.sql
@@ -6,18 +6,33 @@ CREATE TABLE IF NOT EXISTS payment_statuses (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS payments (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    order_id UUID UNIQUE NOT NULL,
-    user_id UUID NOT NULL,
-    amount NUMERIC(10, 2) NOT NULL CHECK (amount > 0),
-    status UUID NOT NULL DEFAULT (SELECT id FROM payment_statuses WHERE name = 'PROCESSING'),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    FOREIGN KEY (status) REFERENCES payment_statuses(id)
-);
-
 INSERT INTO payment_statuses (name)
 VALUES
     ('PROCESSING'),
     ('SUCCEEDED'),
     ('REJECTED');
+
+CREATE TABLE IF NOT EXISTS payments (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_id UUID UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL CHECK (amount > 0),
+    status UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    FOREIGN KEY (status) REFERENCES payment_statuses(id)
+);
+
+CREATE OR REPLACE FUNCTION set_default_payment_status()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.status IS NULL THEN
+        NEW.status := (SELECT id FROM payment_statuses WHERE name = 'PROCESSING');
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_set_default_payment_status
+BEFORE INSERT ON payments
+FOR EACH ROW
+EXECUTE FUNCTION set_default_payment_status();
