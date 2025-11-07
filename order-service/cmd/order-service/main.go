c524f5b3b9b0b66ddd2fe00a74f0385778f806a9
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-11-07 16:09:43 -0300

    feat(order-service): implement gRPC server and client initialization for order service

--- order-service/cmd/order-service/main.go
+++ order-service/cmd/order-service/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net"
 	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
+	orderv1 "github.com/sonuudigital/microservices/gen/order/v1"
+	"github.com/sonuudigital/microservices/order-service/internal/grpc/clients"
+	"github.com/sonuudigital/microservices/order-service/internal/grpc/order"
+	"github.com/sonuudigital/microservices/order-service/internal/repository"
 	"github.com/sonuudigital/microservices/shared/logs"
 	"github.com/sonuudigital/microservices/shared/postgres"
+	"github.com/sonuudigital/microservices/shared/web"
+	"github.com/sonuudigital/microservices/shared/web/health"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -24,4 +36,67 @@ func main() {
 	}
 	logger.Info("database connected successfully")
 	defer pgDb.Close()
+
+	grpcClients, err := initializegRPCClients(logger)
+	if err != nil {
+		logger.Error("failed to initialize gRPC clients", "error", err)
+		os.Exit(1)
+	}
+
+	startGRPCServer(logger, pgDb, grpcClients)
+}
+
+func initializegRPCClients(logger logs.Logger) (*clients.Clients, error) {
+	cartServiceURL := os.Getenv("CART_SERVICE_GRPC_URL")
+	if cartServiceURL == "" {
+		logger.Error("CART_SERVICE_GRPC_URL is not set")
+		os.Exit(1)
+	}
+
+	paymentServiceURL := os.Getenv("PAYMENT_SERVICE_GRPC_URL")
+	if paymentServiceURL == "" {
+		logger.Error("PAYMENT_SERVICE_GRPC_URL is not set")
+		os.Exit(1)
+	}
+
+	clientsURL := clients.NewClienstURL(cartServiceURL, paymentServiceURL)
+	grpcClients, err := clients.NewClients(clientsURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return grpcClients, nil
+}
+
+func startGRPCServer(logger logs.Logger, pgDb *pgxpool.Pool, grpcClients *clients.Clients) {
+	gRPCPort := os.Getenv("ORDER_SERVICE_GRPC_PORT")
+	if gRPCPort == "" {
+		logger.Error("ORDER_SERVICE_GRPC_PORT is not set")
+		os.Exit(1)
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRPCPort))
+	if err != nil {
+		logger.Error("failed to listen on gRPC port", "error", err)
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	orderGrpcServer := order.New(logger, repository.New(pgDb), grpcClients)
+	orderv1.RegisterOrderServiceServer(grpcServer, orderGrpcServer)
+
+	health.StartGRPCHealthCheckService(grpcServer, "order-service", func(ctx context.Context) error {
+		ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+		defer cancel()
+
+		if err := pgDb.Ping(ctx); err == nil {
+			logger.Info("service is healthy and serving")
+			return nil
+		} else {
+			logger.Error("service is not healthy", "error", err)
+			return err
+		}
+	})
+
+	web.StartGRPCServerAndWaitForShutdown(grpcServer, lis, logger)
 }
