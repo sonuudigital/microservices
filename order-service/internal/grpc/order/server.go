7da4d368347534dfce7bcd4069bd8f0dc6beed72
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-11-10 13:48:12 -0300

    feat(order-service): add unit tests for CreateOrder function and refactor server to use RabbitMQPublisher interface

--- order-service/internal/grpc/order/server.go
+++ order-service/internal/grpc/order/server.go
@@ -1,22 +1,27 @@
 package order
 
 import (
+	"context"
+
 	orderv1 "github.com/sonuudigital/microservices/gen/order/v1"
 	"github.com/sonuudigital/microservices/order-service/internal/grpc/clients"
 	"github.com/sonuudigital/microservices/order-service/internal/repository"
 	"github.com/sonuudigital/microservices/shared/logs"
-	"github.com/sonuudigital/microservices/shared/rabbitmq"
 )
+
+type RabbitMQPublisher interface {
+	Publish(ctx context.Context, exchange string, body []byte) error
+}
 
 type Server struct {
 	orderv1.UnimplementedOrderServiceServer
 	logger   logs.Logger
 	querier  repository.Querier
 	clients  *clients.Clients
-	rabbitmq *rabbitmq.RabbitMQ
+	rabbitmq RabbitMQPublisher
 }
 
-func New(logger logs.Logger, querier repository.Querier, clients *clients.Clients, rabbitmq *rabbitmq.RabbitMQ) *Server {
+func New(logger logs.Logger, querier repository.Querier, clients *clients.Clients, rabbitmq RabbitMQPublisher) *Server {
 	return &Server{
 		logger:   logger,
 		querier:  querier,
