c11b13dca7c852e99df6ef832046f1dda6fb9b18
Facundo Ernesto Ortega <facuortega21@gmail.com>
2025-11-23 21:03:17 -0300

    feat(search-service): add http server in main

--- search-service/cmd/search-service/main.go
+++ search-service/cmd/search-service/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/sonuudigital/microservices/search-service/internal/events/consumers/product"
+	productHandler "github.com/sonuudigital/microservices/search-service/internal/handlers/product"
 	"github.com/sonuudigital/microservices/search-service/internal/opensearch"
+	"github.com/sonuudigital/microservices/search-service/internal/router"
 	"github.com/sonuudigital/microservices/shared/logs"
 	"github.com/sonuudigital/microservices/shared/rabbitmq"
+	"github.com/sonuudigital/microservices/shared/web"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -42,15 +49,71 @@ func main() {
 		os.Exit(1)
 	}
 
+	productHandler, err := productHandler.NewProductHandler(logger, opensearchClient, opensearchProductIndex)
+	if err != nil {
+		logger.Error("failed to create product handler", "error", err)
+		os.Exit(1)
+	}
+
+	router, err := router.New(logger, productHandler)
+	if err != nil {
+		logger.Error("failed to create router", "error", err)
+		os.Exit(1)
+	}
+
+	if err := startServices(logger, rabbitmqClient, opensearchClient, opensearchProductIndex, router); err != nil && !errors.Is(err, context.Canceled) {
+		logger.Error("application error", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("application shut down gracefully")
+}
+
+func startServices(logger *logs.SlogLogger, rabbitmqClient *rabbitmq.Client, opensearchClient *opensearch.Client, opensearchProductIndex string, router *router.Router) error {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	go product.
+	g, gCtx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		return startProductEventConsumer(gCtx, logger, rabbitmqClient, opensearchClient, opensearchProductIndex)
+	})
+
+	g.Go(func() error {
+		return startHTTPServer(gCtx, logger, router)
+	})
+
+	return g.Wait()
+}
+
+func startProductEventConsumer(ctx context.Context, logger logs.Logger, rabbitmqClient *rabbitmq.Client, opensearchClient *opensearch.Client, opensearchProductIndex string) error {
+	return product.
 		NewProductEventsConsumer(logger, rabbitmqClient, opensearchClient, opensearchProductIndex).
 		Start(ctx)
+}
 
-	<-ctx.Done()
-	logger.Info("application shut down gracefully")
+func startHTTPServer(ctx context.Context, logger logs.Logger, handler http.Handler) error {
+	srv, err := web.InitializeServer(os.Getenv("PORT"), handler, logger)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		<-ctx.Done()
+		shCtx, shCancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer shCancel()
+		if err := srv.Shutdown(shCtx); err != nil {
+			logger.Error("failed to shutdown server", "error", err)
+		} else {
+			logger.Info("server shutdown complete")
+		}
+	}()
+
+	logger.Info("starting HTTP server", "port", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("HTTP server failed: %w", err)
+	}
+	return nil
 }
 
 func initializeRabbitMQClient(logger logs.Logger) (*rabbitmq.Client, error) {
